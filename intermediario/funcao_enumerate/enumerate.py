354ca20fa82ce742f0122eb9a233c871b92c814a
declau <dec82@hotmail.com>
2021-01-30 20:10:14 -0300

    feat: enumarate

---
+++ intermediario/funcao_enumerate/enumerate.py
@@ -0,0 +1,11 @@
+# Função enumerate
+
+lista = ["Bola", "Banana", "tenis"]
+
+for i in range(len(lista)):
+    print(i, lista[i])
+
+# Pytonico
+print("-------------------")
+for i, nome in enumerate(lista):
+    print(i, nome)
