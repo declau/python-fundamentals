5468b05791d05837cfb057d7249dff05c646de65
declau <dec82@hotmail.com>
2021-02-07 13:40:28 -0300

    filter lambda

--- intermediario/filter/filter.py
+++ intermediario/filter/filter.py
@@ -18,3 +18,6 @@ print(list(filter(long_name, names)))  # cast to list
 
 print([name for name in names if len(name) > 5])  # equivalent list comprehension
 # Out: ['Barney']
+
+arr = [1, 2, 3, 4, 5, 6]
+print([i for i in filter(lambda x: x > 4, arr)])  # outputs[5,6]
