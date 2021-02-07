a3ab77bfe2b81159529cd948ed9ec03fc94e119b
declau <dec82@hotmail.com>
2021-02-07 11:22:37 -0300

    feat: adjust Filter

--- intermediario/filter/filter.py
+++ intermediario/filter/filter.py
@@ -1,4 +1,9 @@
-names = ['Fred', 'Wilma', 'Barney']
+""" FUNCTION: callable that determines the condition or 
+None then use the identity function for filtering (positional- only)
+
+ITERABLE: iterable that will be filtered (positional-only)
+ """
+names = ["Fred", "Wilma", "Barney"]
 
 
 def long_name(name):
@@ -9,4 +14,7 @@ filter(long_name, names)
 # returns a generator
 # Out: <filter at 0x1fc6e443470>
 print(list(filter(long_name, names)))  # cast to list
+# Out: ['Barney']
+
+print([name for name in names if len(name) > 5])  # equivalent list comprehension
 # Out: ['Barney']
