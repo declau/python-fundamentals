bedce2054533a2bca61300894b64e040a49a176b
declau <dec82@hotmail.com>
2021-02-14 21:29:25 -0300

    feat: ordered dict

---
+++ intermediario/collections/ordered_dict.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+
+ordered_dict = {}
+ordered_dict["c"] = 3
+ordered_dict["b"] = 2
+ordered_dict["a"] = 1
+ordered_dict["d"] = 4
+
+print(ordered_dict)
+
+# output: {'c': 3, 'b': 2, 'a': 1, 'd': 4}
