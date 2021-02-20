76a987b3b8e1fcee66ae7c87ab1322c54e5148c8
declau <dec82@hotmail.com>
2021-02-20 19:38:35 -0300

    feat: default dict

---
+++ intermediario/collections/default_dict.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+def_dict = defaultdict(int)
+
+def_dict["a"] = 1
+def_dict["b"] = 2
+
+print(def_dict["a"])
+print(def_dict["c"])
--- intermediario/collections/ordered_dict.py
+++ intermediario/collections/ordered_dict.py
@@ -1,10 +1,12 @@
 from collections import OrderedDict
 
+# ordered_dict = OrderedDict()
+
 ordered_dict = {}
 ordered_dict["c"] = 3
+ordered_dict["d"] = 4
 ordered_dict["b"] = 2
 ordered_dict["a"] = 1
-ordered_dict["d"] = 4
 
 print(ordered_dict)
 
